294f33c843964061e49dafe92fbfbf7ef5ea1c24
Zuzana Sebechlebská <zuzanasebechlebska@gmail.com>
2020-09-22 21:21:01 +0200

    fix of data mining

--- cora/data_mining_cora.py
+++ cora/data_mining_cora.py
@@ -8,13 +8,12 @@ class TupleResult:
     def __init__(self, combination,irrendudant_sums, score):
         
         self.combination=combination
-        if len(irrendudant_sums)==1 & (str(irrendudant_sums[0].system[0].implicant)=='1'):
+        if(len(irrendudant_sums)==1 and (str(irrendudant_sums[0].system[0].implicant)=='1')):
             self.nr_irr_sums=0
             self.min_ir_sum =0
             self.max_ir_sum =0
             self.score=score=0
         else:     
-            
             self.nr_irr_sums = len(irrendudant_sums)
             self.min_ir_sum = min(x.nr_implicants() for x in irrendudant_sums) if self.nr_irr_sums > 0 else 0
             self.max_ir_sum = max(x.nr_implicants() for x in irrendudant_sums) if self.nr_irr_sums > 0 else 0
@@ -31,7 +30,8 @@ def data_mining(data,out_col,len_of_tupple,case_col=None,cut=1,inc1=1,inc2=None,
 
         #print("combination:{}, len_of Comm:{}, x form comb:{}".format(comb,len(comb), [x for x in comb]))
         ir_sums = data_object.get_irredundant_sums()
-        #print("Implicants:{}".format(data_object.prime_implicants))
+        
+
         if len(ir_sums) > 0:
           score = round(max(x.inclusion_score(data, list(comb), out_col[0])*x.coverage_score(data, list(comb), out_col[0]) for x in ir_sums),3)
           tr = TupleResult([x for x in comb],ir_sums, score)
@@ -40,7 +40,6 @@ def data_mining(data,out_col,len_of_tupple,case_col=None,cut=1,inc1=1,inc2=None,
             
             
         else:
-        
           res.append(TupleResult(comb, ir_sums, 0))
       
     return res
